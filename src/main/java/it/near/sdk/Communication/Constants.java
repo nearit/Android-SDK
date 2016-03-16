75857f12cdba6fb8a168b059638f18cc93d95032
Cattaneo Stefano <stefano@nearit.com>
2016-03-16 13:03:19 +0100

    Add volley as a grade dependancy

---
+++ src/main/java/it/near/sdk/Communication/Constants.java
@@ -0,0 +1,7 @@
+package it.near.sdk.Communication;
+
+/**
+ * Created by cattaneostefano on 16/03/16.
+ */
+public class Constants {
+}
--- src/main/java/it/near/sdk/Configuration.java
+++ src/main/java/it/near/sdk/Configuration.java
@@ -3,7 +3,8 @@ package it.near.sdk;
 /**
  * Created by cattaneostefano on 15/03/16.
  */
-public class ConfigurationManager {
+public class Configuration {
+
 
 
 }
