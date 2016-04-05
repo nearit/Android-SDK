e122c4097c86219e6fde9c777828d9a27f0c7ff0
Cattaneo Stefano <stefano@nearit.com>
2016-04-05 20:16:43 +0200

    Add test region crafter

---
+++ src/main/java/it/near/sdk/Beacons/TestRegionCrafter.java
@@ -0,0 +1,37 @@
+package it.near.sdk.Beacons;
+
+import android.content.Context;
+
+import org.altbeacon.beacon.Identifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import it.near.sdk.R;
+
+/**
+ * Created by cattaneostefano on 05/04/16.
+ */
+public class TestRegionCrafter {
+
+    public static ArrayList<Region> getTestRegions(Context context){
+
+        /*ArrayList<Region> testRegions = new ArrayList<>();
+        testRegions.add(new Region("Region" , Identifier.parse("ACFD065E-C3C0-11E3-9BBE-1A514932AC01"), null, null));
+        return testRegions;*/
+
+        ArrayList<Region> testRegions = new ArrayList<>();
+        String[] unparsedStrings = context.getResources().getStringArray(R.array.test_beacons);
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(unparsedStrings));
+        for (String beaconString : list){
+            String[] identifiers = beaconString.split(";");
+            Region testRegion;
+            int major = Integer.parseInt(identifiers[1]);
+            int minor = Integer.parseInt(identifiers[2]);
+            testRegion = new Region("Region" , Identifier.parse(identifiers[0]), Identifier.fromInt(major), Identifier.fromInt(minor) );
+            testRegions.add(testRegion);
+        }
+        return testRegions;
+    }
+}
