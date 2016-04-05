f129d6dc6d9ef20a9af55ec0aad4d8ce2bef79c4
Cattaneo Stefano <stefano@nearit.com>
2016-04-05 20:17:01 +0200

    Rename main notifier

--- src/main/java/it/near/sdk/Rules/NearNotifier.java
+++ src/main/java/it/near/sdk/Rules/NearNotifier.java
@@ -1,10 +1,14 @@
 package it.near.sdk.Rules;
 
+import org.altbeacon.beacon.Region;
+
 import it.near.sdk.Models.Matching;
 
 /**
  * Created by cattaneostefano on 24/03/16.
  */
-public interface MatchingNotifier {
+public interface NearNotifier {
     public abstract void onRuleFullfilled(Matching matching);
+    public abstract void onEnterRegion(Region region);
+    public abstract void onExitRegion(Region region);
 }
