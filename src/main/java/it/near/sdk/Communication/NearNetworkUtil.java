476b8e989c4cdde3ec9ebb85419b6da731fd88d1
Cattaneo Stefano <stefano@nearit.com>
2016-04-20 15:17:54 +0200

    Static utility method for sending trackings

---
+++ src/main/java/it/near/sdk/Communication/NearNetworkUtil.java
@@ -0,0 +1,36 @@
+package it.near.sdk.Communication;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONObject;
+
+import it.near.sdk.GlobalState;
+import it.near.sdk.Utils.ULog;
+
+/**
+ * Created by cattaneostefano on 20/04/16.
+ */
+public class NearNetworkUtil {
+    public static final String TAG = "NearNetworkUtil";
+
+    public static void sendTrack(Context context, String url, String body){
+        GlobalState.getInstance(context).getRequestQueue().add(
+                new CustomJsonRequest(context, Request.Method.POST, url, body, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        ULog.d(TAG, "Tracking data sent.");
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        ULog.d(TAG, "Tracking data not sent. Error: " + error.toString());
+                    }
+                })
+        );
+    }
+
+}
