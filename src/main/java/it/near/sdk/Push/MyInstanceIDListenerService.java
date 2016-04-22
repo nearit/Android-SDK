a5ba1672d70830688dec805308d0c23a8c976190
Cattaneo Stefano <stefano@nearit.com>
2016-04-22 19:15:35 +0200

    Listen for device token changes

---
+++ src/main/java/it/near/sdk/Push/MyInstanceIDListenerService.java
@@ -0,0 +1,25 @@
+package it.near.sdk.Push;
+
+import android.content.Intent;
+
+import com.google.android.gms.iid.InstanceIDListenerService;
+
+import it.near.sdk.GlobalConfig;
+import it.near.sdk.Utils.ULog;
+
+/**
+ * Created by cattaneostefano on 22/04/16.
+ */
+public class MyInstanceIDListenerService extends InstanceIDListenerService {
+
+    private static final String TAG = "MyInstanceIDListenerService";
+
+    @Override
+    public void onTokenRefresh() {
+        super.onTokenRefresh();
+        ULog.d(TAG , "onToken Refresh");
+        Intent intent = new Intent(this, RegistrationIntentService.class);
+        intent.putExtra(RegistrationIntentService.SENDER_ID, GlobalConfig.getInstance(this).getSenderId());
+        startService(intent);
+    }
+}
