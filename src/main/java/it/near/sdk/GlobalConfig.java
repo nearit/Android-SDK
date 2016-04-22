25807dabfebfbc76ca2d12c561988f416736cd71
Cattaneo Stefano <stefano@nearit.com>
2016-04-22 10:33:31 +0200

    Persist global config in local memory

--- src/main/java/it/near/sdk/GlobalConfig.java
+++ src/main/java/it/near/sdk/GlobalConfig.java
@@ -1,6 +1,9 @@
 package it.near.sdk;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+
+import it.near.sdk.Communication.Constants;
 
 /**
  * Created by cattaneostefano on 12/04/16.
@@ -9,12 +12,26 @@ public class GlobalConfig {
 
     private static GlobalConfig mInstance = null;
     private Context mContext;
+    private final String APIKEY = "apikey";
     private String apiKey;
+    private final String APPID = "appid";
     private String appId;
+    private final String NOTIFICATIONIMAGE = "notification_image";
     private int notificationImage = 0;
+    private SharedPreferences sp;
+    private String prefsNameSuffix = "NearConfig";
+    private SharedPreferences.Editor editor;
 
     public GlobalConfig(Context mContext) {
         this.mContext = mContext;
+        setUpSharedPreferences();
+    }
+
+    private void setUpSharedPreferences() {
+        String PACK_NAME = mContext.getApplicationContext().getPackageName();
+        String PREFS_NAME = PACK_NAME + prefsNameSuffix;
+        sp = mContext.getSharedPreferences(PREFS_NAME, 0);
+        editor = sp.edit();
     }
 
     public static GlobalConfig getInstance(Context context){
@@ -26,27 +43,39 @@ public class GlobalConfig {
     }
 
     public int getNotificationImage() {
+        if (notificationImage == 0){
+            notificationImage = sp.getInt(NOTIFICATIONIMAGE, 0);
+        }
         return notificationImage;
     }
 
     public void setNotificationImage(int notificationImage) {
         this.notificationImage = notificationImage;
+        editor.putInt(NOTIFICATIONIMAGE, notificationImage).apply();
     }
 
 
     public String getApiKey() {
+        if (apiKey == null){
+            apiKey = sp.getString(APIKEY, null);
+        }
         return apiKey;
     }
 
     public void setApiKey(String apiKey) {
         this.apiKey = apiKey;
+        editor.putString(APIKEY, apiKey).apply();
     }
 
     public String getAppId() {
+        if (appId == null){
+            appId = sp.getString(APPID, null);
+        }
         return appId;
     }
 
     public void setAppId(String appId) {
         this.appId = appId;
+        editor.putString(APPID, appId);
     }
 }
