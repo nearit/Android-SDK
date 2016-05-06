ffafca870830da3c7b0bf20e81ef4e5075888c7d
Cattaneo Stefano <stefano@nearit.com>
2016-05-06 15:33:41 +0200

    update the poll notification too

--- src/main/java/it/near/sdk/NearItManager.java
+++ src/main/java/it/near/sdk/NearItManager.java
@@ -146,7 +146,7 @@ public class NearItManager {
         forest.refreshConfig();
         simpleNotification.refreshConfig();
         contentNotification.refreshConfig();
-        recipesManager.refreshConfig();
+        pollNotification.refreshConfig();
     }
 
 
