ed23aeb4e7f9c64061a6b91c4685a57e416ce126
Cattaneo Stefano <stefano@nearit.com>
2016-05-16 17:50:17 +0200

    More frequent loading of threshold value

--- src/main/java/it/near/sdk/Beacons/BeaconForest/ForestManager.java
+++ src/main/java/it/near/sdk/Beacons/BeaconForest/ForestManager.java
@@ -188,8 +188,7 @@ public class ForestManager implements BootstrapNotifier {
         // BeaconDynamicRadar radar = new BeaconDynamicRadar(getApplicationContext(), beacons, null);
         // monitor.startRadar(backgroundBetweenScanPeriod, backgroundScanPeriod, regionExitPeriod, regionsToMonitor, this);
         List<Region> superRegions = computeSuperRegions(regionsToMonitor);
-        float threshold = GlobalConfig.getInstance(getApplicationContext()).getThreshold();
-        monitor.startRadar(60000l, 2000l, 20000l, 2000l, regionExitPeriod, threshold, superRegions, regionsToMonitor, this);
+        monitor.startRadar(60000l, 2000l, 20000l, 2000l, regionExitPeriod, superRegions, regionsToMonitor, this);
     }
 
     /**
