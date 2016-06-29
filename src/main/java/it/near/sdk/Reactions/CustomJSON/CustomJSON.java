50efc2ebae2454ae3004654142907c27ed945442
Cattaneo Stefano <stefano@nearit.com>
2016-06-29 14:35:56 +0200

    Initial custom json reaction

---
+++ src/main/java/it/near/sdk/Reactions/CustomJSON/CustomJSON.java
@@ -0,0 +1,11 @@
+package it.near.sdk.Reactions.CustomJSON;
+
+
+import it.near.sdk.MorpheusNear.Resource;
+
+/**
+ * @author cattaneostefano.
+ */
+public class CustomJSON extends Resource {
+
+}
---
+++ src/main/java/it/near/sdk/Reactions/CustomJSON/CustomJSONReaction.java
@@ -0,0 +1,65 @@
+package it.near.sdk.Reactions.CustomJSON;
+
+import java.util.HashMap;
+
+import it.near.sdk.Reactions.CoreReaction;
+import it.near.sdk.Recipes.Models.Recipe;
+
+/**
+ * @author cattaneostefano.
+ */
+public class CustomJSONReaction  {
+/*
+
+
+    private static final String PREFS_SUFFIX = "NearJSON";
+    private static final String PLUGIN_NAME = "custom_json";
+
+    @Override
+    public String getPrefSuffix() {
+        return PREFS_SUFFIX;
+    }
+
+    @Override
+    protected HashMap<String, Class> getModelHashMap() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    protected String getResTypeName() {
+        // TODO
+        return null;
+    }
+
+    @Override
+    public void buildActions() {
+
+    }
+
+    @Override
+    public void refreshConfig() {
+        // TODO
+    }
+
+    @Override
+    public String getPluginName() {
+        return PLUGIN_NAME;
+    }
+
+    @Override
+    protected void handleReaction(String reaction_action, String reaction_bundle, Recipe recipe) {
+
+    }
+
+    @Override
+    public void handlePushReaction(Recipe recipe, String push_id, String bundle_id) {
+
+    }
+
+    @Override
+    public void handleEvaluatedReaction(Recipe recipe, String bundle_id) {
+
+    }
+*/
+}
