3cc8ebd664733aaae3faa98965bc061a8d1513c9
Cattaneo Stefano <stefano@nearit.com>
2016-06-30 10:05:36 +0200

    JSON communication with the app

--- src/main/java/it/near/sdk/Reactions/CoreContentsListener.java
+++ src/main/java/it/near/sdk/Reactions/CoreContentsListener.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 
 import it.near.sdk.Reactions.Content.Content;
 import it.near.sdk.Reactions.Coupon.Coupon;
+import it.near.sdk.Reactions.CustomJSON.CustomJSON;
 import it.near.sdk.Reactions.Poll.Poll;
 
 /**
@@ -15,4 +16,5 @@ public interface CoreContentsListener {
     public abstract void getPollNotification(Intent intent, Poll notification, String notificationText, String content_plugin, String content_action, String pulse_plugin, String pulse_action, String pulse_bundle);
     public abstract void getContentNotification(Intent intent, Content notification, String notificationText, String content_plugin, String content_action, String pulse_plugin, String pulse_action, String pulse_bundle);
     public abstract void getCouponNotification(Intent intent, Coupon notification, String notificationText, String content_plugin, String content_action, String pulse_plugin, String pulse_action, String pulse_bundle);
+    public abstract void getCustomJSONNotification(Intent intent, CustomJSON notification, String notificationText, String content_plugin, String content_action, String pulse_plugin, String pulse_action, String pulse_bundle);
 }
