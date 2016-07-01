114f5024ed59c6bab098b25ff50c542a3daefcf3
Cattaneo Stefano <stefano@nearit.com>
2016-07-01 18:14:50 +0200

    Add classes for BT status trackings

---
+++ src/main/java/it/near/sdk/Utils/OnBluetoothEventsListener.java
@@ -0,0 +1,9 @@
+package it.near.sdk.Utils;
+
+/**
+ * Created by claudiosuardi on 10/02/15.
+ */
+public abstract class OnBluetoothEventsListener {
+    public abstract void onBluetoothOn();
+    public abstract void onBluetoothOff();
+}
