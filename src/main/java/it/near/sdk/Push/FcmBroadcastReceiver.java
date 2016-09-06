e50c61cceeca6be3ddaa578ed7730a9f19ea2f72
Cattaneo Stefano <stefano@nearit.com>
2016-09-06 17:45:15 +0200

    Renaming

--- src/main/java/it/near/sdk/Push/FcmBroadcastReceiver.java
+++ src/main/java/it/near/sdk/Push/FcmBroadcastReceiver.java
@@ -9,11 +9,11 @@ import android.support.v4.content.WakefulBroadcastReceiver;
 /**
  * @author cattaneostefano.
  */
-public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+public class FcmBroadcastReceiver extends WakefulBroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        ComponentName comp = new ComponentName(context.getPackageName(), GcmIntentService.class.getName());
+        ComponentName comp = new ComponentName(context.getPackageName(), FcmIntentService.class.getName());
 
         startWakefulService(context, (intent.setComponent(comp)));
         setResultCode(Activity.RESULT_OK);
