b93eb52b029ba452173e94bcd0a984ff0d24c435
Cattaneo Stefano <stefano@nearit.com>
2016-11-08 13:02:34 +0100

    Parse core contents utils also for parcelable

--- src/main/java/it/near/sdk/Utils/NearUtils.java
+++ src/main/java/it/near/sdk/Utils/NearUtils.java
@@ -2,6 +2,7 @@ package it.near.sdk.Utils;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Parcelable;
 import android.util.Base64;
 
 import org.json.JSONArray;
@@ -23,6 +24,7 @@ import it.near.sdk.Reactions.CustomJSON.CustomJSON;
 import it.near.sdk.Reactions.Feedback.Feedback;
 import it.near.sdk.Reactions.Poll.Poll;
 import it.near.sdk.Reactions.SimpleNotification.SimpleNotification;
+import it.near.sdk.Recipes.Models.Recipe;
 
 /**
  * Near utilities
@@ -139,6 +141,61 @@ public class NearUtils {
                 break;
         }
         return coreContent;
+    }
+
+
+    /**
+     * Parse a parcelable content received from a proximity receiver. Since there's no intent, the intent field of the listener callback methods is always null.
+     * @param content the @{@link Parcelable} content to parse. This contains the object set in the what component of the recipe.
+     * @param recipe the recipe object.
+     * @param listener the listener for the casted content types.
+     * @return true if the content was recognized as core and passed to a callback method, false if it wasn't.
+     */
+    public static boolean parseCoreContents(Parcelable content, Recipe recipe, CoreContentsListener listener) {
+        String reaction_plugin = recipe.getReaction_plugin_id();
+        String reaction_action = recipe.getReaction_action().getId();
+        String notif_body = recipe.getNotificationBody();
+
+        String pulse_plugin = recipe.getPulse_plugin_id();
+        String pulse_action = recipe.getPulse_action().getId();
+        String pulse_bundle = recipe.getPulse_bundle().getId();
+
+        boolean coreContent = false;
+        if (reaction_plugin == null) return false;
+        switch (reaction_plugin) {
+            case "content-notification" :
+                Content c_notif = (Content) content;
+                listener.getContentNotification(null, c_notif, notif_body, reaction_plugin, reaction_action, pulse_plugin, pulse_action, pulse_bundle);
+                coreContent = true;
+                break;
+            case "simple-notification" :
+                SimpleNotification s_notif = (SimpleNotification) content;
+                listener.getSimpleNotification(null, s_notif, notif_body, reaction_plugin, reaction_action, pulse_plugin, pulse_action, pulse_bundle);
+                coreContent = true;
+                break;
+            case "poll-notification" :
+                Poll p_notif = (Poll) content;
+                listener.getPollNotification(null, p_notif, notif_body, reaction_plugin, reaction_action, pulse_plugin, pulse_action, pulse_bundle);
+                coreContent = true;
+                break;
+            case "coupon-blaster" :
+                Coupon coup_notif = (Coupon) content;
+                listener.getCouponNotification(null, coup_notif, notif_body, reaction_plugin, reaction_action, pulse_plugin, pulse_action, pulse_bundle);
+                coreContent = true;
+                break;
+            case "json-sender" :
+                CustomJSON custom_notif = (CustomJSON) content;
+                listener.getCustomJSONNotification(null, custom_notif, notif_body, reaction_plugin, reaction_action, pulse_plugin, pulse_action, pulse_bundle);
+                coreContent = true;
+                break;
+            case "feedbacks" :
+                Feedback f_notif = (Feedback) content;
+                listener.getFeedbackNotification(null, f_notif, notif_body, reaction_plugin, reaction_action, pulse_plugin, pulse_action, pulse_bundle);
+                coreContent = true;
+                break;
+        }
+        return coreContent;
+
     }
 
 
