f51ef82035ddf394a2baf95c839339aec49a3971
Cattaneo Stefano <stefano@nearit.com>
2016-12-02 10:37:49 +0100

    Remove unused utility method

--- src/main/java/it/near/sdk/NearItManager.java
+++ src/main/java/it/near/sdk/NearItManager.java
@@ -209,11 +209,6 @@ public class NearItManager {
         GlobalConfig.getInstance(application).setNotificationImage(imgRes);
     }
 
-
-    public void setThreshold(float threshold) {
-        GlobalConfig.getInstance(application).setThreshold(threshold);
-    }
-
     /**
      * Force the refresh of all SDK configurations.
      */
