4f24377c05e713caba463b12e883e8d60de830d2
Cattaneo Stefano <stefano@nearit.com>
2016-12-06 09:39:33 +0100

    Add nullable to corecontentlistner methods parameters

--- src/main/java/it/near/sdk/Utils/CoreContentsListener.java
+++ src/main/java/it/near/sdk/Utils/CoreContentsListener.java
@@ -1,6 +1,7 @@
 package it.near.sdk.Utils;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 
 import it.near.sdk.Reactions.Content.Content;
 import it.near.sdk.Reactions.Coupon.Coupon;
@@ -15,10 +16,10 @@ import it.near.sdk.Reactions.SimpleNotification.SimpleNotification;
  * @author cattaneostefano
  */
 public interface CoreContentsListener {
-    void getPollNotification(Intent intent, Poll notification, String recipeId);
-    void getContentNotification(Intent intent, Content notification, String recipeId);
-    void getCouponNotification(Intent intent, Coupon notification, String recipeId);
-    void getCustomJSONNotification(Intent intent, CustomJSON notification, String recipeId);
-    void getSimpleNotification(Intent intent, SimpleNotification s_notif, String recipeId);
-    void getFeedbackNotification(Intent intent, Feedback s_notif, String recipeId);
+    void getPollNotification(@Nullable Intent intent, Poll notification, String recipeId);
+    void getContentNotification(@Nullable Intent intent, Content notification, String recipeId);
+    void getCouponNotification(@Nullable Intent intent, Coupon notification, String recipeId);
+    void getCustomJSONNotification(@Nullable Intent intent, CustomJSON notification, String recipeId);
+    void getSimpleNotification(@Nullable Intent intent, SimpleNotification s_notif, String recipeId);
+    void getFeedbackNotification(@Nullable Intent intent, Feedback s_notif, String recipeId);
 }
