016faf86d2fd013c568f9f741225ea0b03ce4be3
Cattaneo Stefano <catt.stefano@gmail.com>
2017-02-28 10:47:08 +0100

    null check in DI

--- src/main/java/it/near/sdk/Geopolis/Trackings/GeopolisTrackingsManager.java
+++ src/main/java/it/near/sdk/Geopolis/Trackings/GeopolisTrackingsManager.java
@@ -1,8 +1,11 @@
 package it.near.sdk.Geopolis.Trackings;
 
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 
 import it.near.sdk.Communication.NearAsyncHttpClient;
+
+import static com.google.gson.internal.$Gson$Preconditions.checkNotNull;
 
 /**
  * Created by cattaneostefano on 27/02/2017.
@@ -13,11 +16,10 @@ public class GeopolisTrackingsManager {
     private final NearAsyncHttpClient nearAsyncHttpClient;
     private final SharedPreferences sp;
 
-    public GeopolisTrackingsManager(NearAsyncHttpClient nearAsyncHttpClient, SharedPreferences sp) {
-        this.nearAsyncHttpClient = nearAsyncHttpClient;
-        this.sp = sp;
+    public GeopolisTrackingsManager(@NonNull NearAsyncHttpClient nearAsyncHttpClient,
+                                    @NonNull SharedPreferences sp) {
+        this.nearAsyncHttpClient = checkNotNull(nearAsyncHttpClient);
+        this.sp = checkNotNull(sp);
     }
-
-
 
 }
