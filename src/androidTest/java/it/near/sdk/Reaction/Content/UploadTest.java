f94844de6c450814a9528b76219551641995a3ff
Cattaneo Stefano <catt.stefano@gmail.com>
2017-03-02 17:12:01 +0100

    - add model for Upload object - add test for parcelable implementation

---
+++ src/androidTest/java/it/near/sdk/Reaction/Content/UploadTest.java
@@ -0,0 +1,40 @@
+package it.near.sdk.Reaction.Content;
+
+import android.os.Parcel;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.google.common.collect.Maps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.HashMap;
+
+import it.near.sdk.Reactions.Content.Upload;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Created by cattaneostefano on 02/03/2017.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class UploadTest {
+
+    @Test
+    public void uploadIsParcelable() {
+        Upload upload = new Upload();
+        upload.setId("upload_id");
+        HashMap<String, Object> map = Maps.newHashMap();
+        String pdfUrl = "http://jsnfijegfuien.pdf";
+        map.put("url", pdfUrl);
+        upload.setUpload(map);
+        Parcel parcel = Parcel.obtain();
+        upload.writeToParcel(parcel, 0);
+        parcel.setDataPosition(0);
+        Upload actual = Upload.CREATOR.createFromParcel(parcel);
+        assertEquals(upload.getId(), actual.getId());
+        assertEquals(upload.getUpload(), actual.getUpload());
+        assertEquals(pdfUrl, actual.getUrl());
+    }
+}
