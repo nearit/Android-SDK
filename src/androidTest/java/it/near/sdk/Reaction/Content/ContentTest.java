10756e47d0fdebf1960ecfd7b4764ad095c6b765
Cattaneo Stefano <catt.stefano@gmail.com>
2017-03-02 17:13:17 +0100

    - add new relationships in Content model - update Content parcel able test

--- src/androidTest/java/it/near/sdk/Reaction/Content/ContentTest.java
+++ src/androidTest/java/it/near/sdk/Reaction/Content/ContentTest.java
@@ -4,13 +4,19 @@ import android.os.Parcel;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import it.near.sdk.Reactions.Content.Audio;
 import it.near.sdk.Reactions.Content.Content;
 import it.near.sdk.Reactions.Content.Image;
 import it.near.sdk.Reactions.Content.ImageSet;
+import it.near.sdk.Reactions.Content.Upload;
 
 import static junit.framework.Assert.*;
 import static org.hamcrest.Matchers.containsInAnyOrder;
@@ -31,6 +37,16 @@ public class ContentTest {
         content.setVideo_link("video_link");
         content.setId("content_id");
         content.setUpdated_at("updated@whatever");
+        Audio audio = new Audio();
+        HashMap<String, Object> audioMap = Maps.newHashMap();
+        audioMap.put("url", "a.mp3");
+        audio.setAudio(audioMap);
+        content.setAudio(audio);
+        Upload upload = new Upload();
+        HashMap<String, Object> uploadMap = Maps.newHashMap();
+        uploadMap.put("url", "a.pdf");
+        upload.setUpload(uploadMap);
+        content.setUpload(upload);
         Parcel parcel = Parcel.obtain();
         content.writeToParcel(parcel, 0);
         parcel.setDataPosition(0);
@@ -40,6 +56,8 @@ public class ContentTest {
         assertEquals(content.getVideo_link(), actual.getVideo_link());
         assertEquals(content.getId(), actual.getId());
         assertEquals(content.getUpdated_at(), actual.getUpdated_at());
+        assertEquals(content.getAudio().getAudio(), actual.getAudio().getAudio());
+        assertEquals(content.getUpload().getUpload(), actual.getUpload().getUpload());
     }
 
 }
