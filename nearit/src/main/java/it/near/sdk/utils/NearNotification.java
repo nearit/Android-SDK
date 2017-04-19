abc33fc2449d31c2b8579c0361ce59ce532bd0cd
Cattaneo Stefano <catt.stefano@gmail.com>
2017-04-19 12:07:31 +0200

    hide explicit notification title, since the app name is always shown anyway

--- nearit/src/main/java/it/near/sdk/utils/NearNotification.java
+++ nearit/src/main/java/it/near/sdk/utils/NearNotification.java
@@ -8,14 +8,27 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
 public class NearNotification {
 
     public static void send(Context context, int imgRes, String title, String message, Intent resultIntent, int code) {
+        NotificationCompat.Builder mBuilder = getBuilder(context, title, message, imgRes, resultIntent);
+
+        Notification notification = new NotificationCompat.BigTextStyle(mBuilder).bigText(message).build();
+        mBuilder.build();
+
+        showNotification(context, code, notification);
+    }
+
+    private static NotificationCompat.Builder getBuilder(Context context,
+                                                         String title,
+                                                         String message,
+                                                         int imgRes,
+                                                         Intent resultIntent) {
         Uri sound_notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context)
-                .setContentTitle(title)
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
                 .setContentText(message)
                 .setContentIntent(getPendingIntent(context, resultIntent))
                 .setSmallIcon(imgRes)
@@ -25,11 +38,14 @@ public class NearNotification {
                 .setSound(sound_notification)
                 .setVibrate(new long[]{100, 200, 100, 500});
 
+        return builderWithTitlePreNougat(builder, title);
+    }
 
-        Notification notification = new NotificationCompat.BigTextStyle(mBuilder).bigText(message).build();
-                mBuilder.build();
-
-        showNotification(context, code, notification);
+    private static NotificationCompat.Builder builderWithTitlePreNougat(NotificationCompat.Builder builder, String title) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N)
+            return builder.setContentTitle(title);
+        else
+            return builder;
     }
 
     private static PendingIntent getPendingIntent(Context context, Intent resultIntent) {
