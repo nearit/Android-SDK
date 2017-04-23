46168b98999167a928612b1668e48f4b6563bc81
Cattaneo Stefano <catt.stefano@gmail.com>
2017-04-23 16:30:17 +0200

    serialization and de-serialization methods for TrackRequest, thoroughly tested

--- nearit/src/main/java/it/near/sdk/trackings/TrackRequest.java
+++ nearit/src/main/java/it/near/sdk/trackings/TrackRequest.java
@@ -1,12 +1,34 @@
 package it.near.sdk.trackings;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 public class TrackRequest {
 
+    static final String KEY_URL = "url";
+    static final String KEY_BODY = "body";
     final String url;
     final String body;
 
     public TrackRequest(String url, String body) {
         this.url = url;
         this.body = body;
+    }
+
+    public JSONObject getJsonObject() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put(KEY_URL, this.url);
+            jsonObject.put(KEY_BODY, this.body);
+        } catch (JSONException ignored) {
+        }
+        return jsonObject;
+    }
+
+    public static TrackRequest fromJsonObject(JSONObject json) throws JSONException {
+        String url = json.getString(KEY_URL);
+        String body = json.getString(KEY_BODY);
+        return new TrackRequest(url, body);
     }
 }
