c5210bdbbb30bcdb712bc14e40ffddfe913ed321
Cattaneo Stefano <catt.stefano@gmail.com>
2017-05-02 11:08:27 +0200

    Deprecate constant for notification title

--- nearit/src/main/java/it/near/sdk/utils/NearItIntentConstants.java
+++ nearit/src/main/java/it/near/sdk/utils/NearItIntentConstants.java
@@ -3,7 +3,6 @@ package it.near.sdk.utils;
 public class NearItIntentConstants {
     public static final String RECIPE_ID = "recipe_id";
     public static final String PUSH_ID = "push_id";
-    public static final String NOTIF_TITLE = "notif_title";
     public static final String NOTIF_BODY = "notif_body";
     public static final String CONTENT = "content";
     public static final String REACTION_PLUGIN = "reaction-plugin";
@@ -11,4 +10,7 @@ public class NearItIntentConstants {
     public static final String PULSE_PLUGIN = "pulse-plugin";
     public static final String PULSE_ACTION = "pulse-action";
     public static final String PULSE_BUNDLE = "pulse-bundle";
+    
+    @Deprecated
+    public static final String NOTIF_TITLE = "notif_title";
 }
