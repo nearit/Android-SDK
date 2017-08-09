bc3db6a0aedadba1bcbe7b4d698887917a3cdbad
Cattaneo Stefano <catt.stefano@gmail.com>
2017-08-09 17:43:02 +0200

    super important recipe triggering behaviour unit test is finally implemented 🛡

--- nearit/src/test/java/it/near/sdk/recipes/RecipesManagerTest.java
+++ nearit/src/test/java/it/near/sdk/recipes/RecipesManagerTest.java
@@ -1,5 +1,8 @@
 package it.near.sdk.recipes;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
 import org.json.JSONException;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -15,15 +18,22 @@ import java.util.List;
 
 import it.near.sdk.TestUtils;
 import it.near.sdk.logging.NearLog;
+import it.near.sdk.recipes.models.PulseAction;
+import it.near.sdk.recipes.models.PulseBundle;
 import it.near.sdk.recipes.models.Recipe;
+import it.near.sdk.recipes.pulse.TriggerRequest;
 import it.near.sdk.recipes.validation.RecipeValidationFilter;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -89,6 +99,113 @@ public class RecipesManagerTest {
         mockSyncRequest(dummyRecipes, true, true);
         recipesManager.syncConfig(recipeRefreshListener);
         verify(recipeRefreshListener, atLeastOnce()).onRecipesRefresh();
+    }
+
+    @Test
+    public void processRecipeRequest_sendsNetworkRequestAndPassListener() {
+        RecipesApi.SingleRecipeListener singleRecipeListener = mock(RecipesApi.SingleRecipeListener.class);
+        String recipeId = "recid";
+        recipesManager.processRecipe(recipeId, singleRecipeListener);
+        verify(recipesApi).fetchRecipe(recipeId, singleRecipeListener);
+    }
+
+    @Test
+    public void triggerRequestForLocalRecipeOnBundle_shouldTriggerWithoutNetworkCalls() {
+        TriggerRequest triggerRequest = new TriggerRequest();
+        triggerRequest.plugin_name = "myplugin";
+        triggerRequest.plugin_action = "myAction";
+        triggerRequest.bundle_id = "mybundle";
+        triggerRequest.plugin_tag_action = "tag_action";
+        triggerRequest.tags = Lists.newArrayList("parappa", "the rapper");
+
+        // prepare a matching recipe in the repository
+        Recipe matchingRecipe = new Recipe();
+        matchingRecipe.setPulse_plugin_id(triggerRequest.plugin_name);
+        PulseAction action = new PulseAction();
+        action.setId(triggerRequest.plugin_action);
+        matchingRecipe.setPulse_action(action);
+        PulseBundle pulseBundle = new PulseBundle();
+        pulseBundle.setId(triggerRequest.bundle_id);
+        matchingRecipe.setPulse_bundle(pulseBundle);
+        matchingRecipe.labels = Maps.newHashMap();
+        when(recipeRepository.getLocalRecipes()).thenReturn(
+                Lists.newArrayList(matchingRecipe)
+        );
+
+        // the recipe is validated
+        when(recipeValidationFilter.filterRecipes(any(List.class))).then(returnsFirstArg());
+
+        recipesManager.handleTriggerRequest(triggerRequest);
+        verifyZeroInteractions(recipesApi);
+        verify(recipeReactionHandler).gotRecipe(matchingRecipe);
+    }
+
+    @Test
+    public void triggerRequestForLocalRecipeOnTags_shouldTriggerWithoutNetworkCalls() {
+        TriggerRequest triggerRequest = new TriggerRequest();
+        triggerRequest.plugin_name = "myplugin";
+        triggerRequest.plugin_action = "myAction";
+        triggerRequest.bundle_id = "mybundle";
+        triggerRequest.plugin_tag_action = "tag_action";
+        triggerRequest.tags = Lists.newArrayList("parappa", "the rapper");
+
+        // prepare matching recipe
+        Recipe matchingRecipe = new Recipe();
+        matchingRecipe.setPulse_plugin_id(triggerRequest.plugin_name);
+        PulseAction action = new PulseAction();
+        action.setId(triggerRequest.plugin_tag_action);
+        matchingRecipe.setPulse_action(action);
+        matchingRecipe.tags = triggerRequest.tags;
+        matchingRecipe.labels = Maps.newHashMap();
+        when(recipeRepository.getLocalRecipes()).thenReturn(
+                Lists.newArrayList(matchingRecipe)
+        );
+
+        // the recipe is validated
+        when(recipeValidationFilter.filterRecipes(any(List.class))).then(returnsFirstArg());
+
+        recipesManager.handleTriggerRequest(triggerRequest);
+        verifyZeroInteractions(recipesApi);
+        verify(recipeReactionHandler).gotRecipe(matchingRecipe);
+    }
+
+    @Test
+    public void triggerRequestForLocalRecipeThatMatchesABundleRecipeAndATagRecipe_shouldOnlyTriggerTheBundleRecipe() {
+        TriggerRequest triggerRequest = new TriggerRequest();
+        triggerRequest.plugin_name = "myplugin";
+        triggerRequest.plugin_action = "myAction";
+        triggerRequest.bundle_id = "mybundle";
+        triggerRequest.plugin_tag_action = "tag_action";
+        triggerRequest.tags = Lists.newArrayList("parappa", "the rapper");
+
+        // prepare matching recipes in the repository
+        Recipe matchingBundleRecipe = new Recipe();
+        Recipe matchingTagRecipe = new Recipe();
+        matchingBundleRecipe.setPulse_plugin_id(triggerRequest.plugin_name);
+        matchingTagRecipe.setPulse_plugin_id(triggerRequest.plugin_name);
+        PulseAction action = new PulseAction();
+        action.setId(triggerRequest.plugin_action);
+        matchingBundleRecipe.setPulse_action(action);
+        PulseAction tagAction = new PulseAction();
+        tagAction.setId(triggerRequest.plugin_tag_action);
+        matchingTagRecipe.setPulse_action(tagAction);
+        PulseBundle pulseBundle = new PulseBundle();
+        pulseBundle.setId(triggerRequest.bundle_id);
+        matchingBundleRecipe.setPulse_bundle(pulseBundle);
+        matchingTagRecipe.tags = triggerRequest.tags;
+        matchingBundleRecipe.labels = Maps.newHashMap();
+        matchingTagRecipe.labels = Maps.newHashMap();
+        when(recipeRepository.getLocalRecipes()).thenReturn(
+                Lists.newArrayList(matchingBundleRecipe, matchingTagRecipe)
+        );
+
+        // the recipes are validated
+        when(recipeValidationFilter.filterRecipes(any(List.class))).then(returnsFirstArg());
+
+        recipesManager.handleTriggerRequest(triggerRequest);
+        verifyZeroInteractions(recipesApi);
+        verify(recipeReactionHandler).gotRecipe(matchingBundleRecipe);
+        verify(recipeReactionHandler, never()).gotRecipe(matchingTagRecipe);
     }
 
     /*@Test
