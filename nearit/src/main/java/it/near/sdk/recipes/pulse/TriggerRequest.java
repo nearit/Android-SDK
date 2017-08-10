645c01009ea3f5c51f9a5224167227ff592f0b48
Cattaneo Stefano <catt.stefano@gmail.com>
2017-08-10 11:19:38 +0200

    new object: tracking info, for bundling recipe id plus extra tracking info

--- nearit/src/main/java/it/near/sdk/recipes/pulse/TriggerRequest.java
+++ nearit/src/main/java/it/near/sdk/recipes/pulse/TriggerRequest.java
@@ -2,10 +2,14 @@ package it.near.sdk.recipes.pulse;
 
 import java.util.List;
 
+import it.near.sdk.trackings.TrackingInfo;
+
 public class TriggerRequest {
     public String plugin_name;
     public String plugin_action;
     public String bundle_id;
     public String plugin_tag_action;
     public List<String> tags;
+
+    public TrackingInfo trackingInfo;
 }
---
+++ nearit/src/main/java/it/near/sdk/trackings/TrackingInfo.java
@@ -0,0 +1,8 @@
+package it.near.sdk.trackings;
+
+import java.util.Map;
+
+public class TrackingInfo {
+    public String recipeId;
+    public Map<String, String> extras;
+}
