07403b55e67e2366de1f9dfcb1db76da00f52f23
Cattaneo Stefano <catt.stefano@gmail.com>
2017-08-11 11:13:54 +0200

    comments on the autotracking receiver

--- nearit/src/main/java/it/near/sdk/recipes/background/AutoTrackingReceiver.java
+++ nearit/src/main/java/it/near/sdk/recipes/background/AutoTrackingReceiver.java
@@ -9,12 +9,19 @@ import it.near.sdk.recipes.models.Recipe;
 import it.near.sdk.trackings.TrackingInfo;
 import it.near.sdk.utils.NearItIntentConstants;
 
+/**
+ * This receiver receives a Near intent, automatically sends an ENGAGED tracking status,
+ * and calls the app launcher activity with the same intent
+ */
 public class AutoTrackingReceiver extends WakefulBroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        // sends tracking
         TrackingInfo trackingInfo = intent.getParcelableExtra(NearItIntentConstants.TRACKING_INFO);
         NearItManager.getInstance(context).sendTracking(trackingInfo, Recipe.ENGAGED_STATUS);
+
+        // sends intent to the launcher activity
         Intent launcherIntent = context.getPackageManager()
                 .getLaunchIntentForPackage(context.getPackageName())
                 .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK)
