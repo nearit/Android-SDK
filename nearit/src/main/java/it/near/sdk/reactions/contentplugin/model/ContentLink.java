6bff1a269af11137e01b1b2ba74162b23caa0115
Cattaneo Stefano <stefano@nearit.com>
2017-10-04 11:30:23 +0200

    make content link parcelable

--- nearit/src/main/java/it/near/sdk/reactions/contentplugin/model/ContentLink.java
+++ nearit/src/main/java/it/near/sdk/reactions/contentplugin/model/ContentLink.java
@@ -1,6 +1,9 @@
 package it.near.sdk.reactions.contentplugin.model;
 
-public class ContentLink {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ContentLink implements Parcelable {
 
     public static final String CTA_LABEL_KEY = "label";
     public static final String CTA_URL_KEY = "url";
@@ -11,5 +14,52 @@ public class ContentLink {
     public ContentLink(String label, String url) {
         this.label = label;
         this.url = url;
+    }
+
+    protected ContentLink(Parcel in) {
+        label = in.readString();
+        url = in.readString();
+    }
+
+    public static final Creator<ContentLink> CREATOR = new Creator<ContentLink>() {
+        @Override
+        public ContentLink createFromParcel(Parcel in) {
+            return new ContentLink(in);
+        }
+
+        @Override
+        public ContentLink[] newArray(int size) {
+            return new ContentLink[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(label);
+        parcel.writeString(url);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContentLink that = (ContentLink) o;
+
+        if (label != null ? !label.equals(that.label) : that.label != null) return false;
+        return url != null ? url.equals(that.url) : that.url == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = label != null ? label.hashCode() : 0;
+        result = 31 * result + (url != null ? url.hashCode() : 0);
+        return result;
     }
 }
