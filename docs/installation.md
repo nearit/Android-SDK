585b60f8a8eabfbcdc58e49cadc1d76edd16ad41
Cattaneo Stefano <stefano@nearit.com>
2018-03-08 13:17:12 +0100

    auto-update from gradle task

--- docs/installation.md
+++ docs/installation.md
@@ -7,7 +7,7 @@ To start using the SDK, include this in your app *build.gradle*
 
 ```java
 dependencies {
-    compile 'it.near.sdk:nearit:2.6.1'
+    compile 'it.near.sdk:nearit:2.6.1_sample'
 }
 ```
 
--- sample/src/main/java/com/nearit/sample/MainActivity.java
+++ sample/src/main/java/com/nearit/sample/MainActivity.java
@@ -1,97 +1,37 @@
 package com.nearit.sample;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+
+import com.nearit.ui_bindings.NearITUIBindings;
+import com.nearit.ui_bindings.permissions.views.PermissionBar;
 
 import it.near.sdk.NearItManager;
-import it.near.sdk.operation.NearItUserProfile;
-import it.near.sdk.reactions.contentplugin.model.Content;
-import it.near.sdk.reactions.couponplugin.model.Coupon;
-import it.near.sdk.reactions.customjsonplugin.model.CustomJSON;
-import it.near.sdk.reactions.feedbackplugin.model.Feedback;
-import it.near.sdk.reactions.simplenotificationplugin.model.SimpleNotification;
-import it.near.sdk.recipes.RecipeRefreshListener;
-import it.near.sdk.trackings.TrackingInfo;
-import it.near.sdk.utils.ContentsListener;
 import it.near.sdk.utils.NearUtils;
 
-public class MainActivity extends AppCompatActivity implements ContentsListener {
+public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-    private static final int NEAR_PERMISSION_REQUEST = 1000;
-    /**
-     * This field key must be mapped on the CMS
-     */
-    private static final String KEY_FOR_AGE_FIELD = "age";
-
-    private boolean isUserLoggedIn = false;
-    private FakeCrm fakeCrm;
-
-    Button loginButton;
-    EditText ageEditText;
+    private PermissionBar bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        fakeCrm = new FakeCrm();
-
-        ageEditText = findViewById(R.id.ageEditText);
-        loginButton = findViewById(R.id.loginButton);
-
-        setLoginButtonText(isUserLoggedIn);
-
+        bar = findViewById(R.id.permission_bar);
+        bar.bindToActivity(this, 6);
     }
 
     public void triggerCustomAction(View view) {
         NearItManager.getInstance().processCustomTrigger("MY_TRIGGER");
     }
 
-    public void requestPermissions(View view) {
-        startActivityForResult(PermissionsActivity.createIntent(MainActivity.this), NEAR_PERMISSION_REQUEST);
-    }
-
-    public void refreshNearRecipes(View view) {
-        NearItManager.getInstance().refreshConfigs(new RecipeRefreshListener() {
-            @Override
-            public void onRecipesRefresh() {
-                Toast.makeText(MainActivity.this, "Refresh successfull", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onRecipesRefreshFail() {
-                Toast.makeText(MainActivity.this, "Could not refresh", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == NEAR_PERMISSION_REQUEST &&
-                resultCode == Activity.RESULT_OK) {
-
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            NearItManager.getInstance().startRadar();
-        }
-    }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        bar.unbindFromActivity();
     }
 
     @Override
@@ -109,96 +49,7 @@ public class MainActivity extends AppCompatActivity implements ContentsListener 
                 NearUtils.carriesNearItContent(intent)) {
             // we got a NearIT intent
 
-            NearUtils.parseContents(intent, this);
+            NearITUIBindings.onNewIntent(this, intent);
         }
-    }
-
-    public void profileMyUser(View view) {
-        NearItManager.getInstance().setUserData(KEY_FOR_AGE_FIELD, (ageEditText.getText().toString()));
-    }
-
-    public void userLogInAndOut(View view) {
-        // this is an example crm integration
-        if (isUserLoggedIn) {
-            logout();
-        } else {
-            login();
-        }
-    }
-
-    private void logout() {
-        fakeCrm.logout();
-        NearItManager.getInstance().resetProfileId(new NearItUserProfile.ProfileFetchListener() {
-            @Override
-            public void onProfileId(String profileId) {
-                // a new empty profile was generated
-            }
-
-            @Override
-            public void onError(String error) {
-                // your local profile was wiped, but no new profile was created
-            }
-        });
-        isUserLoggedIn = false;
-        setLoginButtonText(isUserLoggedIn);
-    }
-
-    private void login() {
-        fakeCrm.login("username", "password", userData -> {
-            isUserLoggedIn = true;
-            setLoginButtonText(isUserLoggedIn);
-            String nearProfileFromServer = userData.nearProfileId;
-            if (nearProfileFromServer == null) {
-                // user needs a near profile
-                NearItManager.getInstance().getProfileId(new NearItUserProfile.ProfileFetchListener() {
-
-                    @Override
-                    public void onProfileId(String profileId) {
-                        fakeCrm.saveProfileId(profileId);
-                    }
-
-                    @Override
-                    public void onError(String error) {}
-                });
-            } else {
-                // user already has a near profile
-                NearItManager.getInstance().setProfileId(nearProfileFromServer);
-            }
-        });
-    }
-
-    private void setLoginButtonText(boolean isUserLoggedIn) {
-        if (isUserLoggedIn) {
-            loginButton.setText("logout");
-        } else {
-            loginButton.setText("login");
-        }
-    }
-
-    @Override
-    public void gotContentNotification(Content notification, TrackingInfo trackingInfo) {
-        Toast.makeText(this, "You received a notification with content", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void gotCouponNotification(Coupon notification, TrackingInfo trackingInfo) {
-        Toast.makeText(this, "You received a coupon", Toast.LENGTH_SHORT).show();
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(notification.getSerial()).create().show();
-    }
-
-    @Override
-    public void gotCustomJSONNotification(CustomJSON notification, TrackingInfo trackingInfo) {
-        Toast.makeText(this, "You received a custom json", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void gotSimpleNotification(SimpleNotification s_notif, TrackingInfo trackingInfo) {
-        Toast.makeText(this, "You received a simple notification: " + s_notif.getNotificationMessage(), Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void gotFeedbackNotification(Feedback s_notif, TrackingInfo trackingInfo) {
-        Toast.makeText(this, "You received a feedback request", Toast.LENGTH_SHORT).show();
     }
 }
